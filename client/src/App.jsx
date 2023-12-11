94cf154d161c5aa12509a52a08bad4eccf260cee
pcstoyle <pcstoyle@hotmail.com>
2023-12-11 12:30:09 -0700

    updated app.jsx, created appolo provider so requests are run through apollo server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,45 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 
 import Navbar from './components/Navbar';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink), // Set up our apollo provider to execute every request to the API server and set the token from localStorage
+  cache: new InMemoryCache(),
+});
+
+
+
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
